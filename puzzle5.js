787002ee14c5d1e41642b04b94ff4b41859e87aa
chelseycampillo <35706456+chelseycampillo@users.noreply.github.com>
2018-01-22 23:31:32 -0800

    Create puzzle5.js

---
+++ puzzle5.js
@@ -0,0 +1,26 @@
+function rightRepeat(nSteps) {
+  var steps = 0;
+  	while (steps < nSteps) {
+      right();
+      steps = steps + 1
+    }
+}
+
+rightRepeat(2);
+var mycolor = getColor();
+if (getColor == "blue") {
+	down();
+} else {
+	up();
+}
+
+rightRepeat(2);
+down();
+rightRepeat(2);
+
+if (getColor == "red") {
+	up();
+} else {
+	down();
+}
+rightRepeat(3);
