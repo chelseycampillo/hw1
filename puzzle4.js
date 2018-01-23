330a0f7f6a2fd0cbac4f55354206e9098a1fbe32
chelseycampillo <35706456+chelseycampillo@users.noreply.github.com>
2018-01-22 23:16:25 -0800

    Create puzzle4.js

---
+++ puzzle4.js
@@ -0,0 +1,18 @@
+var steps = 0;
+	while (steps < 4) {
+    down();
+    steps = steps + 1
+  }
+right();
+var mycolor = getColor();
+down();
+setColor(mycolor);
+right();
+right();
+up();
+right();
+var myColorb = getColor();
+down();
+setColor(myColorb);
+right();
+right();
