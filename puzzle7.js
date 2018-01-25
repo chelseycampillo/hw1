925800bcd04bdeb4ee70ebb6fe78072e22e69a0c
chelseycampillo <35706456+chelseycampillo@users.noreply.github.com>
2018-01-25 10:59:35 -0800

    Update puzzle7.js

--- puzzle7.js
+++ puzzle7.js
@@ -1,111 +1,37 @@
-function downRepeat(nSteps) {
+while (getColor() != "red") {
+  down();
+}
+
+function rightRepeat(nSteps) {
   var steps = 0;
   	while (steps < nSteps) {
-      down();
-    steps = steps + 1
+      right();
+      steps = steps + 1
     }
 }
 
-function upRepeat(nSteps) {
-  var steps = 0;
-  	while (steps < nSteps) {
-      up();
-    steps = steps + 1
-    }
+rightRepeat(2);
+
+while (getColor() != "red") {
+  up();
 }
 
-downRepeat(6);
-var mycolor = getColor();
+rightRepeat(2);
 
-if (getColor() == "red") {
-  right();
-  right();
-} else {
-  down();
+while (getColor() != "red") {
   down();
 }
 
-if (getColor() == "red") {
-  right();
-  right();
-} else {
+rightRepeat(2);
+
+while (getColor() != "red") {
+  up();
+}
+
+rightRepeat(2);
+
+while (getColor() != "red") {
   down();
 }
 
-if (getColor() == "red") {
-  right();
-  right();
-} else {
-  down();
-}
-
-upRepeat(6);
-
-if (getColor() == "red") {
-  right();
-  right();
-} else {
-  up();
-}
-
-if (getColor() == "red") {
-  right();
-  right();
-} else {
-  up();
-  up();
-}
-
-if (getColor() == "red") {
-  right();
-  right();
-} else {
-  up();
-}
-
-downRepeat(8);
-
-if (getColor() == "red") {
-  right();
-  right();
-} else {
-  down();
-}
-
-if (getColor() == "red") {
-  right();
-  right();
-} else {
-  down();
-}
-
-upRepeat(6);
-
-if (getColor() == "red") {
-  right();
-  right();
-  right();
-} else {
-  up();
-  up();
-}
-
-if (getColor() == "red") {
-  right();
-  right();
-  right();
-} else {
-  up();
-}
-
-if (getColor() == "red") {
-  right();
-  right();
-  right();
-} else {
-  up();
-  up();
-  up();
-}
-
-downRepeat(9);
+right();
